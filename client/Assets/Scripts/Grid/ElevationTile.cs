2a791baba4e6b208783ab3983b03bbd098c9af32
Ben Leech <b.leech@hotmail.ca>
2019-01-19 14:25:52 -0600

    start of hex grid system

---
+++ client/Assets/Scripts/Grid/ElevationTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevationTile {
+    Elevation elevation;
+    TileAttribute attribute;
+    Vector3Int localPosition;
+    Vector3Int worldPosition;
+    
+
+    public Elevation Elevation { get; set; }
+
+    public TileAttribute Attribute { get; set; }
+
+    public Vector3Int LocalPosition { get; set; }
+
+    public Vector3Int WorldPosition { get; set; }
+
+}
