f095cc6d99c156a9635f09dfaac3b3076ab901ba
Ben Leech <b.leech@hotmail.ca>
2019-01-21 00:21:39 -0600

    start of level editor

---
+++ client/Assets/Scripts/Grid/TileData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData: MonoBehaviour {
+    int tileId;
+
+    public int TileId { get; set; }
+}
---
+++ client/Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+enum LevelEditorAction {
+    None,
+    Place,
+    Erase,
+}
+
+public class LevelEditor : EditorWindow {
+
+    LevelEditorAction currentAction = LevelEditorAction.None;
+
+    [MenuItem("Window/Level Editor")]
+    public static void ShowWindow() {
+        GetWindow<LevelEditor>("Level Editor");
+    }
+
+    void OnGUI() {
+        GUILayout.Button("Save");
+        GUILayout.Button("Load");
+
+        EditorGUILayout.BeginHorizontal();
+        
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void Save() {
+
+    }
+
+    void Load() {
+
+    }
+}
