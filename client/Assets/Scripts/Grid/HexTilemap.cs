1768d99a0c6b196c23a73a11ae2fad4d0c43e6c4
Ben Leech <b.leech@hotmail.ca>
2019-01-21 11:10:26 -0600

    add tiles to scene

--- client/Assets/Scripts/Grid/HexTilemap.cs
+++ client/Assets/Scripts/Grid/HexTilemap.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class HexTilemap : MonoBehaviour {
 
     Grid grid;
-    
+    Dictionary<Vector3Int,ElevationTile> worldTiles = new Dictionary<Vector3Int, ElevationTile>();
+
+    public bool hasTile(Vector3Int pos) {
+        return worldTiles.ContainsKey(pos);
+    }
+
+    public bool hasTileAtElevation(Vector3Int pos, Elevation el) {
+        ElevationTile tile = null;
+        worldTiles.TryGetValue(pos, out tile);
+        return tile != null && tile.Elevation == el;
+    }
     
 }
--- client/Assets/Scripts/LevelEditor/LevelEditor.cs
+++ client/Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -12,6 +12,7 @@ enum LevelEditorAction {
 public class LevelEditor : EditorWindow {
 
     LevelEditorAction currentAction = LevelEditorAction.None;
+    Grid grid;
 
     [MenuItem("Window/Level Editor")]
     public static void ShowWindow() {
@@ -20,18 +21,55 @@ public class LevelEditor : EditorWindow {
 
     void OnGUI() {
         GUILayout.Button("Save");
-        GUILayout.Button("Load");
 
-        EditorGUILayout.BeginHorizontal();
-        
-        EditorGUILayout.EndHorizontal();
+        DrawUILine(Color.gray);
+        if(GUILayout.Button("Create")) {
+            currentAction = (currentAction == LevelEditorAction.Place) ? LevelEditorAction.None : LevelEditorAction.Place;
+            Debug.Log("Create button clicked. CurrentAction:" + currentAction.ToString());
+        }
+    }
+
+    void OnFocus() {
+        SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
+        SceneView.onSceneGUIDelegate += this.OnSceneGUI;
+    }
+
+    void OnSceneGUI(SceneView sceneView) {
+        Event e = Event.current;
+        int controlId = GUIUtility.GetControlID(FocusType.Passive);
+        switch(e.GetTypeForControl(controlId)) {
+            case EventType.MouseDown:
+                Debug.Log("mouseclick on scene");
+                if(currentAction == LevelEditorAction.Place) {
+                    CreateTile();
+                }
+                break;
+        }
     }
 
     void Save() {
-
+        // saves level as a prefab
     }
 
-    void Load() {
+    void CreateTile() {
+        GameObject tile = PrefabUtility.InstantiatePrefab(Resources.LoadAll("")[0]) as GameObject;
+        Vector3 mousePosition = Event.current.mousePosition;
+        mousePosition.y = SceneView.currentDrawingSceneView.camera.pixelHeight - mousePosition.y;
+        mousePosition = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(mousePosition);
+        mousePosition.y = -mousePosition.y;
+        tile.transform.position = mousePosition;
+    }
 
+    static void DrawUILine(Color color, int thickness = 2, int padding = 10) {
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding+thickness));
+        r.height = thickness;
+        r.y += padding/2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
+    }
+
+    void OnDestroy() {
+        SceneView.onSceneGUIDelegate -= this.OnSceneGUI;
     }
 }
