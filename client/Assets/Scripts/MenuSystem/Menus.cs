7fe95b07809515432e03ee06557d4bb7c9e92087
Bryton Leason <bal983@mail.usask.ca>
2019-01-23 17:00:41 -0600

    Updated menu system location, attached listeners without code

---
+++ client/Assets/Scripts/MenuSystem/Menus.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menus : MonoBehaviour {
+
+    public void StartScreenStartButton() {
+
+    }
+
+    public void LoginScreenLoginButton() {
+
+    }
+
+    public void LoginScreenCreateUserButton() {
+
+    }
+
+    public void LoginScreenQuitButton() {
+
+    }
+
+    public void MainMenuAddFriendButton() {
+
+    }
+
+    public void MainMenuRemoveUserButton() {
+
+    }
+
+    public void MainMenuJoinGameButton() {
+
+    }
+
+    public void MainMenuCreateGameButton() {
+
+    }
+
+    public void MainMenuActiveGamesButton() {
+
+    }
+
+    public void MainMenuGameInvitesButton() {
+
+    }
+
+    public void MainMenuQuitButton() {
+
+    }
+}
