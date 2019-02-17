df4428ebd1b96e4c78ed815d5bc63b6db42370dc
Ben Leech <b.leech@hotmail.ca>
2019-02-17 14:49:30 -0600

    started board controller

---
+++ client/Assets/Scripts/Grid/BoardController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BoardController : MonoBehaviour {
+
+    Tilemap tilemap;
+
+    void Awake(){
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    public Vector3Int WorldToCell(Vector3 position) {
+        return tilemap.WorldToCell(new Vector3(position.x,position.z,0));
+    }
+
+    public Vector3 CellToWorld(Vector3Int position) {
+        Vector3 worldPosition = tilemap.CellToWorld(position);
+        return new Vector3(worldPosition.x,0,worldPosition.y);
+    }
+
+    public HexTile GetHexTile(Vector3Int position) {
+        return tilemap.GetTile(position) as HexTile;
+    }
+}
