cec1f858d774ac3ed068f1c8055fae86eac97380
kad996 <kad996@mail.usask.ca>
2019-03-02 14:51:06 -0600

    "Ravine"

--- client/Assets/Scripts/Grid/BoardMetadata.cs
+++ client/Assets/Scripts/Grid/BoardMetadata.cs
@@ -4,6 +4,7 @@
 public enum BoardType {
     //small maps from 0 to 99
     Hawkpoint = 1,
+	Ravine = 2,
 
     //medium maps from 100 to 199
     Pinnacle = 100,
@@ -17,7 +18,8 @@ public enum BoardType {
 
 public static class BoardMetadata {
     public static Dictionary<BoardType, int> MaxPlayersDict = new Dictionary<BoardType, int>() {
-        { BoardType.Hawkpoint, 3 },
+        { BoardType.Ravine, 2},
+		{ BoardType.Hawkpoint, 3 },
         { BoardType.Pinnacle, 4 },
         { BoardType.Valley, 4 },
         { BoardType.SampleMap, 6 }
@@ -25,6 +27,7 @@ public static class BoardMetadata {
 
     public static Dictionary<BoardType, int> CostDict = new Dictionary<BoardType, int>() {
         { BoardType.Hawkpoint, 15 },
+		{ BoardType.Ravine, 15 },
         { BoardType.Pinnacle, 25 },
         { BoardType.Valley, 25 },
         { BoardType.SampleMap, 40 }
@@ -32,6 +35,7 @@ public static class BoardMetadata {
 
     public static Dictionary<BoardType, string> BoardNames = new Dictionary<BoardType, string>() {
         { BoardType.Hawkpoint, "Map_Small_Hawkpoint" },
+		{ BoardType.Ravine, "Map_Small_Ravine" },
         { BoardType.Pinnacle, "Map_Med_Pinnacle" },
         { BoardType.Valley, "Map_Med_Valley" },
         { BoardType.SampleMap, "SampleMap" }
