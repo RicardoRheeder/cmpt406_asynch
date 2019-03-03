59a727f06880a3cb00d87664960c6da0e2856439
kad996 <kad996@mail.usask.ca>
2019-03-03 01:08:05 -0600

    Made tab targetting script for text fields
    Only put it on login menu for now

---
+++ client/Assets/Scripts/MenuSystem/Tabbing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Tabbing : MonoBehaviour
+{
+    public Selectable nextField;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            nextField.Select();
+    }
+}
