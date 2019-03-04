cce2cf9b5e631fc6cb46d9f56aa6f20a7680afc9
Ben Leech <b.leech@hotmail.ca>
2019-03-04 15:34:09 -0600

    started fog of war system

---
+++ client/Assets/Scripts/FogOfWarController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FogOfWarController : MonoBehaviour {
+
+    Tilemap fogTilemap;
+    Tilemap baseTilemap;
+
+    public void InitializeFogOfWar(Tilemap tilemap) {
+        if(tilemap == null) {
+            return;
+        }
+
+        if(fogTilemap == null) {
+            GameObject newTilemap = new GameObject();
+            newTilemap.AddComponent(typeof(Tilemap));
+            fogTilemap = newTilemap.GetComponent<Tilemap>();
+        }
+        fogTilemap.transform.SetParent(tilemap.transform.parent);
+        fogTilemap.transform.position = tilemap.transform.position;
+    }
+
+    public void ClearFogAtPosition(Vector2Int position) {
+
+    }
+
+}
---
+++ client/Assets/Scripts/Grid/FogTile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class FogTile : TileBase {
+    [HideInInspector]
+    public GameObject tileModel;
+    public Sprite spritePreview;
+
+    GameObject tileObject;
+
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject go) {
+        if(go) {
+            go.transform.rotation = Quaternion.Euler(0,0,0);
+            tileObject = go;
+        }
+
+        #if UNITY_EDITOR
+        if (go != null){
+            if (go.scene.name == null) {
+                Debug.Log("DestroyImmediate");
+                DestroyImmediate(go);
+            }
+        }
+        #endif
+
+        return true;
+    }
+
+    public override void RefreshTile(Vector3Int location, ITilemap tilemap) {
+        base.RefreshTile(location,tilemap);
+    }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData) {
+        tileData.sprite = spritePreview; // Do I need a sprite for it to be detected?
+        tileData.colliderType = Tile.ColliderType.Grid;
+        tileData.flags = TileFlags.None;
+        tileData.gameObject = tileModel;
+    }
+
+    public GameObject GetTileObject() {
+        return tileObject;
+    }
+}
