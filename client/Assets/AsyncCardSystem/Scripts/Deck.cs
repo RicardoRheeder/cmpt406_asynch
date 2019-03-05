51fea84e2938a8f3b675146d6f88908e8efc5e38
Ricardo Rheeder (rir954) <rir954@mail.usask.ca>
2019-03-05 14:00:46 -0600

    Merge process

---
+++ client/Assets/AsyncCardSystem/Scripts/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+
+[CreateAssetMenu(fileName = "New Deck", menuName = "Deck")]
+public class Deck : ScriptableObject
+{
+    [SerializeField]
+    public List<Card> deck;
+
+    public void printDeck()
+    {
+        Debug.Log("Deck Size: " + deck.Count);
+        int count = 1;
+        foreach (Card card in deck)
+        {
+            Debug.Log(count + ": " +card.name);
+            count++;
+        }
+    }
+
+    public int count()
+    {
+        return deck.Count;
+    }
+}
