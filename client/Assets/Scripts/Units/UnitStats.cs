26d724f92aa7ff332e7b0a9644d9c1b94a5211bf
Ben Leech <b.leech@hotmail.ca>
2019-03-07 12:17:57 -0600

    moving units along a path

--- client/Assets/Scripts/Units/UnitStats.cs
+++ client/Assets/Scripts/Units/UnitStats.cs
@@ -121,7 +121,7 @@ public class UnitStats {
         if(!specialMove)
             this.MovementActions--;
         this.Position = position;
-        MyUnit.transform.position = board.CellToWorld(position);
+        MyUnit.MoveTo(position,board);
     }
 
     //We need to convert the xPos and yPos variables to be Position
