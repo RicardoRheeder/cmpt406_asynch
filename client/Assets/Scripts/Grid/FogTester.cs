f0dfe636af1d50735b800398d500d5f46597dcbf
Ben Leech <b.leech@hotmail.ca>
2019-03-08 00:28:32 -0600

    created FogTester

---
+++ client/Assets/Scripts/Grid/FogTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogTester : MonoBehaviour {
+
+    BoardController boardController;
+    FogOfWarController fogOfWarController;
+
+    void Awake(){
+        fogOfWarController = GetComponent<FogOfWarController>();
+        boardController = new BoardController();
+    }
+
+    void Start() {
+        fogOfWarController.InitializeFogOfWar(boardController.GetTilemap());
+    }
+
+    void Update() {
+        if(Input.GetMouseButton(0)) {
+            fogOfWarController.ClearFogAtPosition(boardController.MousePosToCell());
+        }
+    }
+}
