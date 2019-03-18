cb724d66af248bd87f36572653cadd9df13ba8aa
GauravMenon <gauravmenon37@gmail.com>
2019-03-17 20:56:11 -0600

    Changed the enum SoundName to be in it's own file

---
+++ client/Assets/Scripts/Audio/SoundName.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum SoundName {
+        Theme,
+        ButtonPress,
+        ButtonError,
+        ButtonQuit,
+
+        TrooperMove,
+        TrooperAttack,
+        TrooperDeath,
+
+        ReconMove,
+        ReconAttack,
+        ReconDeath,
+
+        SteamerMove,
+        SteamerAttack,
+        SteamerDeath,
+
+        PewpewMove,
+        PewpewAttack,
+        PewpewDeath,
+
+        CompensatorMove,
+        CompensatorAttack,
+        CompensatorDeath,
+
+        TungstenMove,
+        TungstenAttack,
+        TungstenDeath,
+
+        SandmanMove,
+        SandmanAttack,
+        SandmanDeath,
+
+        FoundationMove,
+        FoundationAttack,
+        FoundationDeath,
+
+        PowersurgeMove,
+        PowersurgeAttack,
+        PowersurgeDeath,
+
+        MidasMove,
+        MidasAttack,
+        MidasDeath,
+
+        ClaymoreMove,
+        ClaymoreAttack,
+        ClaymoreDeath,
+
+        AlbarnMove,
+        AlbarnAttack,
+        AlbarnDeath,
+
+        AdrenMove,
+        AdrenAttack,
+        AdrenDeath
+}
