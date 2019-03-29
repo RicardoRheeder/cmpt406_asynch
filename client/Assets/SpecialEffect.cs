1db20720ddedbab8d0c4b1c4358cfe75ce13e771
Ricardo Rheeder (rir954) <rir954@mail.usask.ca>
2019-03-29 17:57:20 -0600

    Fixed Compensator and prepared sections for everyone

--- client/Assets/SpecialEffect.cs
+++ client/Assets/SpecialEffect.cs
@@ -1,23 +1,155 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 public class SpecialEffect : MonoBehaviour
 {
+    private UnitType unitType;
+
     public Transform particleEffect;
     public Transform startPoint;
+    private float distance;
 
-    // Update is called once per frame
     public void PlayAttackEffect(Vector3 sourceWorldPos, Vector3 targetWorldPos) {
         GameObject gm = Instantiate(particleEffect, this.transform).gameObject;
         gm.transform.SetParent(this.transform);
-
-        float distance = Vector3.Distance(sourceWorldPos, targetWorldPos);
-        Debug.Log(distance);
-
+        distance = Vector3.Distance(sourceWorldPos, targetWorldPos);
         gm.transform.localPosition = new Vector3(startPoint.position.x, startPoint.position.y, startPoint.position.z);
 
-        Destroy(gm, 3f);
+
+        //        switch (unitType)
+        //        {
+        //            case UnitType.compensator:
+        //                VECompensatorAttack();
+        //                break;
+        //        }
+
+        //------------------------------------------
+        /**
+        *  TODO: RICARDO's SECTION START
+        */
+        VECompensatorAttack();
+
+
+
+
+
+        /**
+         *  RICARDO's SECTION END
+        */
+        //------------------------------------------
+        /**
+         *  TODO: GAURAV's SECTION START
+         */
+        VEPewPewAttack();
+
+
+
+
+        /**
+         *  GAURAV's SECTION END
+        */
+        //------------------------------------------
+        /**
+        *  TODO: CLAYTON's SECTION START
+        */
+
+
+
+
+
+        /**
+         *  CLAYTONS's SECTION END
+        */
+        //------------------------------------------
+        /**
+        *  TODO: JAAMI's SECTION START
+        */
+
+
+
+
+
+        /**
+         *  JAAMI's SECTION END
+        */
+        //------------------------------------------
+
+        Destroy(gm, 3f); //Destroys the Visual Effect
     }
+
+
+    //------------------------------------------
+    /**
+    *  RICARDO's SECTION START
+    */
+    private void VECompensatorAttack()
+    {
+        StartCoroutine(findMakeObject());
+    }
+
+    IEnumerator findMakeObject()
+    {
+        GameObject MakeObject = null;
+        while (MakeObject == null)
+        {
+            MakeObject = GameObject.Find("MakeObject");
+            yield return new WaitForSeconds(0.1f);
+        }
+        print("FOUND");
+        MakeObject.transform.localPosition = new Vector3(0, 0, distance);
+    }
+
+    private void VECardEffect() { }
+
+    private void VEDeath() { }
+
+    /**
+     *  RICARDO's SECTION END
+    */
+    //------------------------------------------
+    /**
+     *  GAURAV's SECTION START
+     */
+    void VEPewPewAttack()
+    {
+        particleEffect.GetComponent<TranslateMove>().m_fowardMove = false;
+        particleEffect.GetComponent<TranslateMove>().m_upMove = true;
+        particleEffect.GetComponent<TranslateMove>().m_rightMove = false;
+
+    }
+
+
+
+
+    /**
+     *  GAURAV's SECTION END
+    */
+    //------------------------------------------
+    /**
+    *  CLAYTON's SECTION START
+    */
+
+
+
+
+
+    /**
+     *  CLAYTONS's SECTION END
+    */
+    //------------------------------------------
+    /**
+    *  JAAMI's SECTION START
+    */
+
+
+
+
+
+    /**
+     *  JAAMI's SECTION END
+    */
+    //------------------------------------------
 }
